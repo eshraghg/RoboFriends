03f5489379bb9095b08d051272dfd450331bea41
Eshargh Ghoodjani <e.ghoodjani@gmail.com>
2019-12-31 23:04:43 +0330

    cardList created

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { robots } from './robots';
+
+const Card = (props) => {
+    return (
+        <div className='bg-light-green dib br3 pa3 ma2 grow ba2 shadow-5 tc'>
+            <img src={`https://robohash.org/${props.id}?100x100`} alt='robo pic'></img>
+            <div>
+                <h1>{props.name}</h1>
+                <p>{props.email}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import Hello from './Hello';
+import Cardlist from './Cardlist';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots'
 
-ReactDOM.render(<h1>Hi sweetie</h1>, document.getElementById('root'));
+ReactDOM.render(
+                <Cardlist robots={robots}/>
+                ,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
